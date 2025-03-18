dd12f43f705db906a25fcbccb30fbab954ca28f2
Lucas Lio <laguiliolucas@gmail.com>
2025-03-18 09:22:11 -0300

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,2 +1,38 @@
-const NomeMateria = "Frameworks I"
+
+let variavel = 10;
+let numero:number = 10;
+let string:string = "Isso é uma string";
+const boleano = true //false
+let vetorString:string[] = [] 
+let NomeMateria:string = "FrameWorks I"
+const vetor:number[] = [1] 
+
+
+// Adicionar um valor no vetor
+
+vetorString.push("Lucas")
+vetor.push(2,3)
 console.log(NomeMateria)
+console.log(vetor, vetorString)
+
+// Objetos em javascript
+
+const objEstudante = {
+    Nome: "Lucas", 
+    Idade: 16,
+    CPF: "053.575.17.100",
+
+}
+
+objEstudante.CPF = "'12345678910"
+console.log(objEstudante)
+
+// Crie um vetor com dois objetos, nome, idade,cpf 
+
+const vetorobj2Estudante = {
+    nome: ["Lucas", "Thamires"],
+    idade: [16,16],
+    cpf: ["05357517100", "07979252101"],
+} 
+
+console.log(vetorobj2Estudante)
